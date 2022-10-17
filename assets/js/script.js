3939ef43bbc5e8b5ff763555d511af59bb28c5fe
Diego Delgado Barra <diego.delgadobarra@outlook.com>
2022-10-17 01:19:44 -0300

    Tipos de mensajes y variables especiales

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+// Mensajes por consola
+console.log('mensaje enviado por consola');
+console.warn('mensaje de alerta por consola');
+console.error('Error por consola');
+
+
+
+// Mensajes por el navegador
+document.write('<h1>Muestra lo que se desea por el navegador como titulo </h1>');
+document.write('<h2>Muestra lo que se desea por el navegador</h2>');
+
+
+// Mensajes emergentes
+alert('mensaje emergente informativo');
+prompt('Mensaje emergente para solicitar datos');
+confirm('Mensaje emergente para solicitar confirmación');
+
+
+
+
+
+
